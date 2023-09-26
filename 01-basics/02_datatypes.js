4808b3927cdbe59d96504ac0f4a63c2ff5cc2f79
Sourav Dey <102379824+Srv228@users.noreply.github.com>
2023-09-26 05:53:22 +0000

    datatypes

---
+++ 01-basics/02_datatypes.js
@@ -0,0 +1,32 @@
+"use strict"; //treat all JS Code as newer Version
+
+
+// alert( 3 + 3) //we are using nodeJS not browser
+
+console.log(3 + 
+    3);    //code readability should be high
+
+console.log("Sourav");
+
+
+let name = "Sourav"
+let age = 27
+let isLoggedIn = true
+
+
+
+//number => 2 to power 53
+// bigInt
+// string  => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique (mostly use in React)
+
+
+
+
+// Object
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
