5bf050c661deddccc058f73e34e6cd07e386b68c
Sourav Dey <102379824+Srv228@users.noreply.github.com>
2023-09-28 05:57:20 +0000

    String

---
+++ 01-basics/05_String.js
@@ -0,0 +1,43 @@
+const name = "Sourav"
+const repoCount = 50
+
+// console.log(name + repoCount + "Value"); //Outdated , we use bactics(String Interpolation)
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+
+const gameName = new String('Sourav-srv')
+
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+
+console.log(gameName.length); //9
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('r'));
+
+
+const newString = gameName.substring(0, 4);
+console.log(newString);
+
+
+const anotherString = gameName.slice(-8,4)
+console.log(anotherString);
+
+
+const newStringOne = "   Sourav   "
+
+console.log(newString.trim());
+
+
+
+
+
+
+
+
+
+
+
